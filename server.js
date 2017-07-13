5a7de749a19cd0a15f7843dda1256c1d4347ff24
Savannah Cosby <savannah.cosby123@gmail.com>
2017-07-13 14:25:28 -0400

    hopefull again

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ application.set('port', process.env.PORT || 3000);
 application.use(express.static(__dirname + '/public'));
 
 application.get('/', function (req, res) {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile("/index.html");
 });
 
 application.listen(application.get('port'), () => {
